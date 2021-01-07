2da3458af1a316de741f05450c1a5d34dfe4529e
Dragomir98 <dragomir.stoychev98@gmail.com>
2021-01-08 00:39:41 +0200

    Added webform, taxonomy and paragraph

---
+++ web/sites/default/files/php/twig/5fda299fb61b3_search-result.html.twig_Xl__tU_KrbOpUIa9JIAFgIT82/JLM6-Miq2R8mogRC9dUKFRBMznKbhF873qP7KsZVaFU.php
@@ -0,0 +1,128 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/bartik/templates/classy/content/search-result.html.twig */
+class __TwigTemplate_0fdd5b363a3c375206015936cf96e5a0cbd410a96c92eca7014f2cc92c5929e4 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = array("if" => 66);
+        $filters = array("escape" => 59);
+        $functions = array("attach_library" => 59);
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                ['attach_library']
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 59
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\Core\Template\TwigExtension']->attachLibrary("bartik/classy.search-results"), "html", null, true);
+        echo "
+";
+        // line 60
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_prefix"] ?? null), 60, $this->source), "html", null, true);
+        echo "
+<h3";
+        // line 61
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["title_attributes"] ?? null), "addClass", [0 => "search-result__title"], "method", false, false, true, 61), 61, $this->source), "html", null, true);
+        echo ">
+  <a href=\"";
+        // line 62
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["url"] ?? null), 62, $this->source), "html", null, true);
+        echo "\">";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null), 62, $this->source), "html", null, true);
+        echo "</a>
+</h3>
+";
+        // line 64
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title_suffix"] ?? null), 64, $this->source), "html", null, true);
+        echo "
+<div class=\"search-result__snippet-info\">
+  ";
+        // line 66
+        if (($context["snippet"] ?? null)) {
+            // line 67
+            echo "    <p";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["content_attributes"] ?? null), "addClass", [0 => "search-result__snippet"], "method", false, false, true, 67), 67, $this->source), "html", null, true);
+            echo ">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["snippet"] ?? null), 67, $this->source), "html", null, true);
+            echo "</p>
+  ";
+        }
+        // line 69
+        echo "  ";
+        if (($context["info"] ?? null)) {
+            // line 70
+            echo "    <p class=\"search-result__info\">";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["info"] ?? null), 70, $this->source), "html", null, true);
+            echo "</p>
+  ";
+        }
+        // line 72
+        echo "</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/bartik/templates/classy/content/search-result.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  105 => 72,  99 => 70,  96 => 69,  88 => 67,  86 => 66,  81 => 64,  74 => 62,  70 => 61,  66 => 60,  62 => 59,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/bartik/templates/classy/content/search-result.html.twig", "C:\\wamp64\\www\\drupal-project\\web\\core\\themes\\bartik\\templates\\classy\\content\\search-result.html.twig");
+    }
+}
