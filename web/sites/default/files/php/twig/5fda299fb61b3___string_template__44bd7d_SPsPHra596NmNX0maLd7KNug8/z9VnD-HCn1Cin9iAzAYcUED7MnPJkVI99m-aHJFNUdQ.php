e6f9c82516514a10c1e8a9bb110d74a2b6bdf35d
Dragomir98 <dragomir.stoychev98@gmail.com>
2021-01-09 01:00:53 +0200

    Added sidebars for model and manufacturer occurunces

---
+++ web/sites/default/files/php/twig/5fda299fb61b3___string_template__44bd7d_SPsPHra596NmNX0maLd7KNug8/z9VnD-HCn1Cin9iAzAYcUED7MnPJkVI99m-aHJFNUdQ.php
@@ -0,0 +1,87 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__44bd7d505bd205e3a44d812044dc00fe40c975a9f370735a9f2ed9eb7df7f898 */
+class __TwigTemplate_caee72441ac25b2892444310963129800a98cff14b1e385cadb0ab5983f61e28 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = array();
+        $filters = array("escape" => 1);
+        $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["name"] ?? null), 1, $this->source), "html", null, true);
+        echo " (";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["nid"] ?? null), 1, $this->source), "html", null, true);
+        echo ")";
+    }
+
+    public function getTemplateName()
+    {
+        return "__string_template__44bd7d505bd205e3a44d812044dc00fe40c975a9f370735a9f2ed9eb7df7f898";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  62 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__44bd7d505bd205e3a44d812044dc00fe40c975a9f370735a9f2ed9eb7df7f898", "");
+    }
+}
