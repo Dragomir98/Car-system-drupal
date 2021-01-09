49137a23259ccfb404e7f7e74549ac757f68a576
Dragomir98 <dragomir.stoychev98@gmail.com>
2021-01-09 12:23:24 +0200

    Added changes to manufacturer vocabulary and car data content type

---
+++ web/sites/default/files/php/twig/5fda299fb61b3_webform-container-base-ht_CUj1fykQwbioISWSZ4rLdpReu/2Z9MvCND0n4bRx5wbjvtA8HjIrM-wXvtZ2DYRsxAdsw.php
@@ -0,0 +1,86 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/webform/templates/webform-container-base-html.html.twig */
+class __TwigTemplate_1857271da072bac12a64fc7b3fe4fa7f521a3fdd535b0ecc9610b1fc0ae1fcaa extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = array();
+        $filters = array("escape" => 14);
+        $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 14
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["value"] ?? null), 14, $this->source), "html", null, true);
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/webform/templates/webform-container-base-html.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  62 => 14,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/contrib/webform/templates/webform-container-base-html.html.twig", "C:\\wamp64\\www\\drupal-project\\web\\modules\\contrib\\webform\\templates\\webform-container-base-html.html.twig");
+    }
+}
